639db1187316b4e781a3004cfd0097dd344bcb50
ramiro-l <ramideporte@gmail.com>
2024-11-27 13:28:58 -0300

    feat: refact file components.

--- components/tool/container-tool.tsx
+++ components/tool/container-tool.tsx
@@ -2,35 +2,35 @@
 
 import { RepositoryProvider, useRepository } from "@/context/RepositoryContext"
 
-import CardContents from "@/components/tool/card-contents"
-import CardDownload from "@/components/tool/card-download"
-import CardInfoRepo from "@/components/tool/card-info-repo"
+import CardContainer from "@/components/tool/container/card-container"
+import CardDownload from "@/components/tool/download/card-download"
+import CardInfoRepo from "@/components/tool/info-repo/card-info-repo"
 import InputRepo from "@/components/tool/input-repo"
 
 function Tool() {
     const { loading, container } = useRepository()
+    const isLoadingOrEmpty = loading || container.length === 0
 
     return (
         <>
             <div
                 className={`transition-all duration-200 transform z-50 ${
-                    loading || container.length === 0
-                        ? "translate-y-20"
-                        : "-translate-y-0 "
+                    isLoadingOrEmpty ? "translate-y-20" : "-translate-y-0"
                 }`}
             >
                 <InputRepo />
             </div>
             <div
-                className={`flex flex-col relative gap-2 transition-all duration-300 transform ${
-                    loading || container.length === 0
-                        ? "opacity-0 -translate-y-5 scale-90 h-0"
-                        : "opacity-100 translate-y-0 scale-100"
-                }`}
+                className={`flex flex-col relative gap-2 transition-all duration-300 transform 
+                    ${
+                        isLoadingOrEmpty
+                            ? "opacity-0 -translate-y-5 scale-90 h-0"
+                            : "opacity-100 translate-y-0 scale-100"
+                    }`}
             >
                 <CardInfoRepo />
-                <div className="flex gap-2 !max-h-[85vh] ">
-                    <CardContents />
+                <div className="flex gap-2 !max-h-[85vh]">
+                    <CardContainer />
                     <CardDownload />
                 </div>
             </div>
--- components/tool/download/info-selected-files.ts
+++ components/tool/download/info-selected-files.ts
@@ -1,28 +1,4 @@
-"use client"
-
-import { useRepository } from "@/context/RepositoryContext"
-import { DownloadIcon } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
-
-export default function CardDownload() {
-    const { cantFilesSelected, cantFoldersSelected } = useRepository()
-    return (
-        <div className="border flex flex-col justify-center rounded-xl p-10 dark:bg-background bg-slate-100 w-1/3">
-            <p className=" text-lg font-semibold text-center ">
-                Download Content
-            </p>
-            <Button className="mt-2">
-                <DownloadIcon size={24} />
-            </Button>
-            <p className="text-center text-sm py-1 rounded-md text-secondary-foreground/75">
-                {infoSelectedFiles(cantFilesSelected, cantFoldersSelected)}
-            </p>
-        </div>
-    )
-}
-
-function infoSelectedFiles(
+export function infoSelectedFiles(
     cantFilesSelected: number,
     cantFoldersSelected: number
 ): string {
