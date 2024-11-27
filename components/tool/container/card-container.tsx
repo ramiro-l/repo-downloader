491493288a510b1f738e7a7cac30e3500a971061
ramiro-l <ramideporte@gmail.com>
2024-11-27 13:29:44 -0300

    feat: refact file components.

--- components/tool/container/card-container.tsx
+++ components/tool/container/card-container.tsx
@@ -3,14 +3,14 @@
 import { useRepository } from "@/context/RepositoryContext"
 
 import { Tree } from "@/components/ui/file-tree"
+import ElementsTree from "@/components/tool/container/elements-tree"
 import DotsLoading from "@/components/tool/dots-loading"
-import ElementsTree from "@/components/tool/elements-tree"
 
-export default function CardContents() {
+export default function CardContainer() {
     const { container, toggleSelectItem, selectFolder, loading } =
         useRepository()
     return (
-        <div className="flex items-center border rounded-xl px-1 py-2 dark:bg-background bg-slate-100 w-2/3">
+        <div className="flex items-center border border-input rounded-xl px-1 py-2 dark:bg-background bg-slate-100 w-2/3">
             {container.length === 0 || loading ? (
                 <div className="w-full flex justify-center">
                     <DotsLoading />
