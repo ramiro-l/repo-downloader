13731afceca9c9f3977049ec44ca1dd09b467954
ramiro-l <ramideporte@gmail.com>
2024-12-05 19:02:54 -0300

    feat: add page for fetch branch.

--- services/github/get-branches.ts
+++ services/github/get-branches.ts
@@ -1,26 +1,41 @@
 import { GITHUB_API_URL } from "@/services/github/const"
 import { handleGithubApiRateLimitError } from "@/services/github/get-rate-limit"
 
+const BRANCHES_PER_PAGE = 100
+
 export const getGithubBranches = async (
     owner: string,
     repo: string
 ): Promise<string[]> => {
-    const response = await fetch(
-        `${GITHUB_API_URL}/repos/${owner}/${repo}/branches`
-    )
+    let page = 1
+    const allBranches: string[] = []
 
-    if (!response.ok) {
-        handleGithubApiRateLimitError((await response.json()).message)
-        throw new Error("Failed to get branches")
+    while (true) {
+        const response = await fetch(
+            `${GITHUB_API_URL}/repos/${owner}/${repo}/branches?per_page=${BRANCHES_PER_PAGE}&page=${page}`
+        )
+        const branches = await response.json()
+
+        if (!response.ok) {
+            handleGithubApiRateLimitError(branches.message)
+            throw new Error("Failed to get branches")
+        }
+
+        if (
+            !Array.isArray(branches) ||
+            !branches.every((branch) => typeof branch.name === "string")
+        ) {
+            throw new Error("Invalid response from GitHub API")
+        }
+
+        allBranches.push(...branches.map((branch) => branch.name as string))
+
+        if (branches.length < BRANCHES_PER_PAGE) {
+            break
+        }
+
+        page++
     }
 
-    const branches = await response.json()
-    if (
-        !Array.isArray(branches) ||
-        !branches.every((branch: any) => typeof branch.name === "string")
-    ) {
-        throw new Error("Invalid response from GitHub API")
-    }
-
-    return branches.map((branch: any) => branch.name as string)
+    return allBranches
 }
