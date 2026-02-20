3d4447abe5fcf6daf14b618034fdf11f1a218b42
Ramiro Lugo <74385260+ramiro-l@users.noreply.github.com>
2026-02-20 00:52:07 -0300

    Remove donation page redirect configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/donate/cancel',
-        destination: '/',
-        permanent: false,
-      },
-      {
-        source: '/donate/completed',
-        destination: '/',
-        permanent: false,
-      },
-
-    ];
-  },
 }
 
 export default nextConfig
